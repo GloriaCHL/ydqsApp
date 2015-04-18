57569a56a6e4dc4cd74d4a2e86c74b5340e413d8
GloriaCHL <lichuhua12@gmail.com>
2015-04-18 11:50:56 +0800

    '2015/4/18'

--- public/server/scripts/directives/contTabs.js
+++ public/server/scripts/directives/contTabs.js
@@ -12,7 +12,7 @@ define(['app'],function(app){
                         +'<li ng-class="{active:$index==tabIndex}" ng-repeat="tab in tabsList"><a href="javascript:;" ng-click="changeToParent($index,tab.target)">{{tab.text}}</a></li>'
                         +'</ul>',
             controller:['$scope',function($scope){
-                $scope.changeToParent = function(index,target){console.log(target)
+                $scope.changeToParent = function(index,target){
                     $scope.$parent.tabChange(index,target);
                 }
             }]
